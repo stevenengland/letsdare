023b04d75caaeee26165225ecb3c60c669791026
letsdare <github@steven-england.info>
2023-05-03 20:44:35 +0200

    chore: ramp up

---
+++ app/common/services/logging_service.py
@@ -0,0 +1,21 @@
+import structlog
+
+from app.common.services.logging_service_interface import (
+    LoggingServiceInterface,
+)
+
+
+class LoggingService(LoggingServiceInterface):
+    def __init__(
+        self,
+    ) -> None:
+        self.logger = structlog.get_logger("django")
+
+    def log_info(self, message: str) -> None:
+        self.logger.info(message)
+
+    def log_warning(self, message: str) -> None:
+        self.logger.warning(message)
+
+    def log_error(self, message: str) -> None:
+        self.logger.error(message)
