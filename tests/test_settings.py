8f54b4f7b9678d103cff495a1f0b4175f4c74095
letsdare <github@steven-england.info>
2023-05-06 08:44:41 +0200

    chore: rampup

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,4 +4,4 @@ from django.conf import settings
 
 
 def test_all_app_settings_are_set() -> None:
-    assert settings.PHOTOS_REPO_ROOTDIR  # type: ignore[misc]
+    assert settings.LOGGING_LEVEL  # type: ignore[misc]
