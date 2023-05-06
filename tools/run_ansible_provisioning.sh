0ca53c982199291ab024627a5aece2d14950c4df
letsdare <github@steven-england.info>
2023-05-06 09:11:52 +0200

    chore: rampup

--- tools/run_ansible_provisioning.sh
+++ tools/run_ansible_provisioning.sh
@@ -3,4 +3,4 @@
 BASEDIR="$(readlink -f $0 | xargs dirname)/.."
 
 ansible-galaxy install --force -r $BASEDIR/tools/ansible_runbook/requirements.yml
-ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml
+ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml --ask-become-pass --extra-vars "@$BASEDIR/tools/ansible_runbook/.playbook_overrides.yml"
