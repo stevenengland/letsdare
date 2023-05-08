abf5b8e6454731e3b9eec83cecdc03b980847990
letsdare <github@steven-england.info>
2023-05-08 10:02:55 +0200

    feat: add become flag -b if needed

--- tools/run_ansible_provisioning.sh
+++ tools/run_ansible_provisioning.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 BASEDIR="$(readlink -f $0 | xargs dirname)/.."
-ASK_BECOME_PASS=""
+BECOME_FLAG=""
 
 if [ "$EUID" -ne 0 ]; then
-   ASK_BECOME_PASS="--ask-become-pass"
+   BECOME_FLAG="--ask-become-pass"
+fi
+
+if [ -n "${PW_LESS_SUDO}" ] && [ "${PW_LESS_SUDO}" = true  ];then
+  BECOME_FLAG="-b"
 fi
 
 ansible-galaxy install --force -r $BASEDIR/tools/ansible_runbook/requirements.yml
 
 if test -f $BASEDIR/tools/ansible_runbook/.playbook_overrides.yml; then
-  ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml $ASK_BECOME_PASS --extra-vars "@$BASEDIR/tools/ansible_runbook/.playbook_overrides.yml"
+  ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml $BECOME_FLAG --extra-vars "@$BASEDIR/tools/ansible_runbook/.playbook_overrides.yml"
 else
-  ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml $ASK_BECOME_PASS
+  ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml $BECOME_FLAG
 fi
