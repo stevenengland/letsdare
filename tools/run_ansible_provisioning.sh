317a8cfd50245892635d4c1013cb00f73ecff04d
letsdare <github@steven-england.info>
2023-05-08 09:04:27 +0200

    feat(improve ansible script): make askpass variable and test for overrides

--- tools/run_ansible_provisioning.sh
+++ tools/run_ansible_provisioning.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 BASEDIR="$(readlink -f $0 | xargs dirname)/.."
+ASK_BECOME_PASS=""
+
+if [ "$(id -u)" != "0" ]; then
+   ASK_BECOME_PASS="--ask-become-pass"
+fi
 
 ansible-galaxy install --force -r $BASEDIR/tools/ansible_runbook/requirements.yml
-ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml --ask-become-pass --extra-vars "@$BASEDIR/tools/ansible_runbook/.playbook_overrides.yml"
+
+if test -f $BASEDIR/tools/ansible_runbook/.playbook_overrides.yml; then
+  ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml $ASK_BECOME_PASS --extra-vars "@$BASEDIR/tools/ansible_runbook/.playbook_overrides.yml"
+else
+  ansible-playbook $BASEDIR/tools/ansible_runbook/prepare_machine.yml $ASK_BECOME_PASS
+fi
